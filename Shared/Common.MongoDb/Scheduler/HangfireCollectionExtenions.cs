0b86eef9e60bc89e0fa3baf57b949307f4f5a33b
Anton Romanyuk <a.romanyuk@smartcat.ai>
2022-01-02 19:41:22 +0500

    add prometheus

--- Shared/Common.MongoDb/Scheduler/HangfireCollectionExtenions.cs
+++ Shared/Common.MongoDb/Scheduler/HangfireCollectionExtenions.cs
@@ -4,6 +4,7 @@ using Hangfire.Mongo.Migration.Strategies;
 using Hangfire.Mongo.Migration.Strategies.Backup;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
 namespace Common.MongoDb.Scheduler
 {
@@ -13,21 +14,26 @@ namespace Common.MongoDb.Scheduler
             this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("Hangfire");
+            var mongoUrlBuilder = new MongoUrlBuilder(connectionString);
+            var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+
+            var storageOptions = new MongoStorageOptions
+            {
+                MigrationOptions = new MongoMigrationOptions
+                {
+                    MigrationStrategy = new MigrateMongoMigrationStrategy(),
+                    BackupStrategy = new CollectionMongoBackupStrategy()
+                },
+                Prefix = "hangfire.mongo",
+                CheckConnection = true
+            };
+
             services.AddHangfire(config =>
             {
                 config
                     .UseSimpleAssemblyNameTypeSerializer()
                     .UseRecommendedSerializerSettings()
-                    .UseMongoStorage(connectionString, new MongoStorageOptions
-                    {
-                        MigrationOptions = new MongoMigrationOptions
-                        {
-                            MigrationStrategy = new MigrateMongoMigrationStrategy(),
-                            BackupStrategy = new CollectionMongoBackupStrategy()
-                        },
-                        Prefix = "hangfire.mongo",
-                        CheckConnection = true
-                    });
+                    .UseMongoStorage(mongoClient, mongoUrlBuilder.DatabaseName, storageOptions);
             });
 
             services.AddHangfireServer();
