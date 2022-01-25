24604278fce2b425c20789b1bcbf656179da329c
Anton Romanyuk <a.romanyuk@smartcat.ai>
2022-01-25 19:23:21 +0500

    add tests

--- Shared/Common.Logging/CorrelationContext.cs
+++ Shared/Common.Logging/CorrelationContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading;
-
-namespace Common.Logging
+﻿namespace Common.Logging
 {
     /// <summary>
     /// Correlation context.
---
+++ Shared/Tests/CorrelationContextTests.cs
@@ -0,0 +1,50 @@
+﻿using Common.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class CorrelationContextTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public CorrelationContextTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        private async Task Function()
+        {
+            TrySetCorrelationId("function 1");
+
+            await Task.Delay(1);
+
+            TrySetCorrelationId("function 2");
+        }
+
+        private void TrySetCorrelationId(string id)
+        {
+            try
+            {
+                CorrelationContext.CorrelationId = id;
+                _testOutputHelper.WriteLine(CorrelationContext.CorrelationId);
+            }
+            catch (Exception exception)
+            {
+                _testOutputHelper.WriteLine(exception.Message);
+                _testOutputHelper.WriteLine(CorrelationContext.CorrelationId);
+            }
+        }
+
+        [Fact]
+        public async Task CorrelationContextTest()
+        {
+            TrySetCorrelationId("base");
+
+            await Function();
+
+            _testOutputHelper.WriteLine("Task run");
+            await Task.Run(() => Function());
+        }
+    }
+}
